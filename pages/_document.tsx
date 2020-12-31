5689030ef0aa71a527555e7625154d497969c393
elee <ericlee.2311@gmail.com>
2020-12-31 01:51:09 -0500

    add open graph

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,13 @@ export default class CustomDocument extends Document {
     render() {
         return (
             <Html lang="en-US">
-                <Head/>
+                <Head>
+                    <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet"/>
+                    <meta property="og:title" content="SpaceX Mission Log"/>
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="http://localhost:3000/"/>
+                    <meta property="og:image" content="" />
+                </Head>
                 <body>
                     <Main/>
                     <NextScript/>
