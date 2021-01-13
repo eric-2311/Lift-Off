1d4e04b03d725aa24315c9f298cc05ae0a069fad
elee <ericlee.2311@gmail.com>
2021-01-13 14:42:40 -0500

    attempt og image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,8 +8,8 @@ export default class CustomDocument extends Document {
                     <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet"/>
                     <meta property="og:title" content="SpaceX Mission Log"/>
                     <meta property="og:type" content="website" />
-                    <meta property="og:url" content="http://localhost:3000/"/>
-                    <meta property="og:image" content="%PUBLIC_URL%/meta-pic.png" />
+                    <meta property="og:url" content="https://lift-off.vercel.app/"/>
+                    <meta property="og:image" content="https://lift-off.vercel.app/" />
                     <meta property="og:description" content="Browse previous SpaceX launches with this app built using Contentful and Next.js. View launch videos and data from each launch via Contentful CMS!"/>
                 </Head>
                 <body>
