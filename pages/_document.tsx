a8849b4abfab8cd99be952e9962b53246e7756cf
elee <ericlee.2311@gmail.com>
2021-01-22 19:30:54 -0500

    image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default class CustomDocument extends Document {
                     <meta property="og:title" content="SpaceX Mission Log"/>
                     <meta property="og:type" content="website" />
                     <meta property="og:url" content="https://lift-off.vercel.app/"/>
-                    <meta property="og:image" content="" />
+                    <meta property="og:image" content="%PUBLIC_URL%/meta-pic.png" />
                     <meta property="og:description" content="Browse previous SpaceX launches with this app built using Contentful and Next.js. View launch videos and data from each launch via Contentful CMS!"/>
                 </Head>
                 <body>
